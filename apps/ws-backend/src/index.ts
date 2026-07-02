5b940baeb113d7a82c453db7aebaa47fb676528a
shashikiran6sk <shashikiran6.sk@gmail.com>
2026-07-03 01:05:30 +0530

    added endpoints

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,23 @@
 import {WebSocketServer} from "ws";
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if(!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (!decoded) {
+    ws.close();
+    return;
+  }
+  
   ws.on("message", function message(data) {
     ws.send("something");
   });
