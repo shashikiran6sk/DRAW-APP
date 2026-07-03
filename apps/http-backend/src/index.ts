7c8068a455df2f449504890cf083c54f809ecfff
shashikiran6sk <shashikiran6.sk@gmail.com>
2026-07-03 23:49:48 +0530

    added common packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema} from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
-    //db call
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Invalid request body",
+        });
+        return;
+    }
     res.json({
         userId: 1,
     })
