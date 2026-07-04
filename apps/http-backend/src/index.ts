0739029138942fa2efb7ae82ee24f6b5738edfff
shashikiran6sk <shashikiran6.sk@gmail.com>
2026-07-05 01:04:41 +0530

    added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,22 +3,35 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import {CreateUserSchema} from "@repo/common/types";
-// import { prismaClient } from "@repo/db/client";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
 
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
         res.json({
             message: "Invalid request body",
         });
         return;
     }
-    res.json({
-        userId: 1,
-    })
+    try {
+        await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                password: parsedData.data?.password,
+                name: parsedData.data?.name,
+            }
+        });
+        res.json({
+            userId: "123",
+        })
+    } catch (error) {
+        res.status(411).json({
+            message: "User already exists",
+        })
+    }
 })
 
 app.post("/signin", (req, res) => {
