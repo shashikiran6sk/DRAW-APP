bf304717a96356de8ce3b45ce09b114f275782c5
shashikiran6sk <shashikiran6.sk@gmail.com>
2026-07-05 01:32:43 +0530

    add business logic to endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -6,6 +6,7 @@ import {CreateUserSchema} from "@repo/common/types";
 import { prismaClient } from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
 
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prismaClient = new PrismaClient();
+export const prismaClient = new PrismaClient({
+   adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
+});
